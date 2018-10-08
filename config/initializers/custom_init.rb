53a0275faaf32ce2ead87bc53d3a824f44cd4d41
Voxoff <guybennettjones@hotmail.co.uk>
2018-10-08 14:49:14 +0100

    initialiser code

--- config/initializers/custom_init.rb
+++ config/initializers/custom_init.rb
@@ -1,6 +1,23 @@
-
+# class Watch
+#   def updated?
+#     puts "New Watch"
+#   end
+# end
 # initializer :my_custom_preparation do |app|
+    puts "aghj"
+    app = Rails.application
+    app.reloaders << app.config.file_watcher.new(["app/views/restaurants/index.html.erb"]) do 
+      puts "ghj"
+    end
 ActiveSupport::Reloader.to_prepare do
     puts "Reloading now ..."
-  end
-# end
+
+    # app.reloaders << Watch.new
+    # app.reloaders << ActiveSupport::FileUpdateChecker.new([], { "app/views" => [".erb", ".html.erb"]}) do
+      # reload!
+    # end
+    # app.reloaders << ActiveSupport::FileUpdateChecker.new(["app/views/restaurants/index.html.erb"]) do
+    #   app.reload!
+    # end
+
+end
