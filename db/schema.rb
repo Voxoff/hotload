70532c6b69ac743a703e3190f8571f103546c0ca
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-12 21:00:47 +0000

    switched to pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_20_162114) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
